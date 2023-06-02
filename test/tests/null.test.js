11864d28d168201bd3dfad6c3eff6381a99fe45e
Saria <saria@saria.fun>
2023-06-02 18:49:53 -0400

    Created null-case test suite

---
+++ test/tests/null.test.js
@@ -0,0 +1,10 @@
+describe('Test when the script does effectively nothing', () => {
+	test('it adds the event listener', async () => {
+	});
+
+	test('it does not change DOM content', async () => {
+	});
+
+	test('it does not fetch anything', async () => {
+	});
+});
