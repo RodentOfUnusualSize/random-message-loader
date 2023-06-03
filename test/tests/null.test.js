00fa06cf44ce8b1ba4f310a3bea041d8dcb468c1
Saria <saria@saria.fun>
2023-06-03 16:26:19 -0400

    Test that event listeners are not added

--- test/tests/null.test.js
+++ test/tests/null.test.js
@@ -1,9 +1,25 @@
 describe('When the script does effectively nothing', () => {
+	windowEventListeners = new Map();
+	documentEventListeners = new Map();
+
 	beforeAll(() => {
+		window.addEventListener = jest.fn((event, cb) => {
+			if (!windowEventListeners.has(event))
+				windowEventListeners.set(event, []);
+			windowEventListeners.get(event).push(cb);
+		});
+		document.addEventListener = jest.fn((event, cb) => {
+			if (!documentEventListeners.has(event))
+				documentEventListeners.set(event, []);
+			documentEventListeners.get(event).push(cb);
+		});
+
 		fetch.mockClear();
 	});
 
 	test('it does not add event listener', async () => {
+		expect(windowEventListeners.size).toBe(0);
+		expect(documentEventListeners.size).toBe(0);
 	});
 
 	test('it does not change DOM content', async () => {
