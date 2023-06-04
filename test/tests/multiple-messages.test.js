64413f29dfecb0ef61189691348805a4a2dc0813
Saria <saria@saria.fun>
2023-06-03 21:31:44 -0400

    Check fetch calls and element content

--- test/tests/multiple-messages.test.js
+++ test/tests/multiple-messages.test.js
@@ -22,8 +22,27 @@
 describe('When multiple messages are wanted from a single source', () => {
 	const scriptPath = '../../src/random-message-loader.js';
 
+	const url = 'messages';
+
+	const messages = Array.from(Array(3).keys())
+		.map(n => `message #${n}`)
+	;
+
+	const elements = [];
+
+	beforeEach(() => {
+		jest.resetModules();
+
+		fetch.mockClear();
+		fetch.mockResponse(messages.join('\n'));
+	});
+
 	test('content of all target elements is changed', async () => {
 		await require(scriptPath);
+
+		for (const element of elements) {
+			expect(element.innerHTML).toMatch(/^message #[0-9]+$/);
+		}
 	});
 
 	test('content of all target elements is randomly selected', async () => {
@@ -32,5 +51,10 @@ describe('When multiple messages are wanted from a single source', () => {
 
 	test('only one fetch request is made', async () => {
 		await require(scriptPath);
+
+		expect(fetch.mock.calls).toBeArrayOfSize(1);
+
+		expect(fetch.mock.calls[0]).toBeArrayOfSize(1);
+		expect(fetch.mock.calls[0][0]).toBe(url);
 	});
 });
