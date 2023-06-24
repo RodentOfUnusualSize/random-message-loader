b2e7a470dd0a725234183bb8c80789c481625495
Saria <saria@saria.fun>
2023-06-23 21:16:06 -0400

    Created test suite for basic message ID use

---
+++ test/tests/message-id.test.js
@@ -0,0 +1,37 @@
+/***********************************************************************
+ *                                                                     *
+ * This file is part of Saria’s random message loader program.         *
+ *                                                                     *
+ * Saria’s random message loader program is free software: you can     *
+ * redistribute it and/or modify it under the terms of the GNU         *
+ * General Public License as published by the Free Software            *
+ * Foundation, either version 3 of the License, or (at your option)    *
+ * any later version.                                                  *
+ *                                                                     *
+ * Saria’s random message loader program is distributed in the hope    *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the  *
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     *
+ * PURPOSE. See the GNU General Public License for more details.       *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with Saria’s random message loader program.                   *
+ * If not, see <https: *www.gnu.org/licenses/>.                        *
+ *                                                                     *
+ **********************************************************************/
+
+describe('When a message ID is used', () => {
+	const scriptPath = '../../src/random-message-loader.js';
+
+	afterEach(() => {
+		saria.testing.jsdom.restore();
+
+		jest.restoreAllMocks();
+		jest.resetModules();
+	});
+
+	test.todo('it changes the content of the target element');
+
+	test.todo('it fetches the message file');
+
+	test.todo('the same message is used in every element');
+});
