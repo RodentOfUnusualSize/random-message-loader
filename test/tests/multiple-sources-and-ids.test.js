6fec5eb13862b9e39f6d756aee7d4a9c5496e39d
Saria <saria@saria.fun>
2023-06-24 15:37:46 -0400

    Created test

--- test/tests/multiple-sources-and-ids.test.js
+++ test/tests/multiple-sources-and-ids.test.js
@@ -29,7 +29,75 @@ describe('When multiple message sources and IDs are used', () => {
 		jest.resetModules();
 	});
 
-	test.todo('each source and ID pair gets the same message');
+	test('each source and ID pair gets the same message', async () => {
+		const urls = ['url-1', 'url-2', 'url-3'];
+		const ids = ['id-1', 'id-2', 'id-3'];
+
+		const sources = new Map(
+			urls.map(url => [url, Array.from(Array(10).keys()).map(i => `${url}: #${i}`)])
+		);
+
+		const cartesian = (...a) => a.reduce((b, c) => b.flatMap(d => c.map(e => [...d, e])), [[]]);
+		const pairs = cartesian(urls, ids);
+
+		document.head.innerHTML = `<script src="${scriptPath}"></script>`;
+
+		const groups = pairs
+			.map(([url, id]) => {
+				const group = {
+					'url'      : url,
+					'id'       : id,
+					'message'  : null,
+					'elements' : [],
+				};
+
+				const ul = document.createElement('ul');
+				document.body.append(ul);
+
+				for (let i = 0; i < 3; ++i) {
+					const li = document.createElement('li');
+					li.setAttribute('data-saria-random-message-src', group.url);
+					li.setAttribute('data-saria-random-message-id', group.id);
+					ul.append(li);
+					group.elements.push(li);
+				}
+
+				return group;
+		});
+
+		jest.spyOn(globalThis, 'fetch')
+			.mockImplementation(url => {
+				if (sources.has(url)) {
+					return Promise.resolve({
+						'url'        : url,
+						'ok'         : true,
+						'status'     : 200,
+						'statusText' : 'Ok',
+						'text'       : jest.fn().mockResolvedValue(sources.get(url).join('\n')),
+					});
+				}
+				else {
+					return Promise.reolve({
+						'url'        : url,
+						'ok'         : false,
+						'status'     : 404,
+						'statusText' : 'Not Found',
+					});
+				}
+			});
+
+		const completion = new Promise(resolve => document.addEventListener('saria:random-message-loader:done', () => resolve()));
+		require(scriptPath);
+		await completion;
+
+		groups.forEach(group => group.message = group.elements[0].innerHTML);
+
+		groups.forEach(group => {
+			group.elements.forEach(element => {
+				expect(element.innerHTML).toBe(group.message);
+			});
+		});
+	});
 
 	test.todo('only a single fetch is made for each URL');
 });
