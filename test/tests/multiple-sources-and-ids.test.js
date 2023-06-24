38d4b108822ff9c9dca01e883acc526bf87d67a7
Saria <saria@saria.fun>
2023-06-24 14:36:35 -0400

    Created test suite for using multiple message sources and IDs

---
+++ test/tests/multiple-sources-and-ids.test.js
@@ -0,0 +1,35 @@
+/***********************************************************************
+ *                                                                     *
+ * This file is part of Saria’s random message loader program.         *
+ *                                                                     *
+ * Saria’s random message loader program is free software: you can     *
+ * redistribute it and/or modify it under the terms of the GNU         *
+ * General Public License as published by the Free Software            *
+ * Foundation, either version 3 of the License, or (at your option)    *
+ * any later version.                                                  *
+ *                                                                     *
+ * Saria’s random message loader program is distributed in the hope    *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the  *
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     *
+ * PURPOSE. See the GNU General Public License for more details.       *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with Saria’s random message loader program.                   *
+ * If not, see <https: *www.gnu.org/licenses/>.                        *
+ *                                                                     *
+ **********************************************************************/
+
+describe('When multiple message sources and IDs are used', () => {
+	const scriptPath = '../../src/random-message-loader.js';
+
+	afterEach(() => {
+		saria.testing.jsdom.restore();
+
+		jest.restoreAllMocks();
+		jest.resetModules();
+	});
+
+	test.todo('each source and ID pair gets the same message');
+
+	test.todo('only a single fetch is made for each URL');
+});
