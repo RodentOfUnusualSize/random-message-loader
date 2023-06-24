81c17d0f4b3ee219b22d8f7c02cc61083bf99c7c
Saria <saria@saria.fun>
2023-06-24 13:49:48 -0400

    Created test

--- test/tests/multiple-ids.test.js
+++ test/tests/multiple-ids.test.js
@@ -29,7 +29,61 @@ describe('When multiple message IDs are used', () => {
 		jest.resetModules();
 	});
 
-	test.todo('elements with each ID have the same message');
+	test('elements with each ID have the same message', async () => {
+		const url = 'url';
+
+		document.head.innerHTML = `<script src="${scriptPath}"></script>`;
+
+		const ids = ['id-1', 'id-2', 'id-3'];
+
+		const idGroups = ids.map(id => {
+			return {
+				'id'       : id,
+				'message'  : null,
+				'elements' : [],
+			};
+		});
+
+		const table = document.createElement('table');
+		document.body.append(table);
+		table.innerHTML = '<thead><tr>'
+			+ idGroups.map(idGroup => `<th>${idGroup.id}</th>`).join('')
+			+ '</tr></thead>'
+		;
+
+		const tbody = document.createElement('tbody');
+		table.append(tbody);
+		for (let i = 0; i < 3; ++i) {
+			const tr = document.createElement('tr');
+			tbody.append(tr);
+
+			idGroups.forEach(idGroup => {
+				const td = document.createElement('td');
+				td.setAttribute('data-saria-random-message-src', url);
+				td.setAttribute('data-saria-random-message-id', idGroup.id);
+				tr.append(td);
+				idGroup.elements.push(td);
+			});
+		}
+
+		jest.spyOn(globalThis, 'fetch')
+			.mockResolvedValue({
+				ok   : true,
+				text : jest.fn().mockResolvedValue(Array.from(Array(10).keys()).map(n => `message <b>${n}</b>`).join('\n')),
+			});
+
+		const completion = new Promise(resolve => document.addEventListener('saria:random-message-loader:done', () => resolve()));
+		require(scriptPath);
+		await completion;
+
+		idGroups.forEach(idGroup => idGroup.message = idGroup.elements[0].innerHTML);
+
+		idGroups.forEach(idGroup => {
+			idGroup.elements.forEach(element => {
+				expect(element.innerHTML).toBe(idGroup.message);
+			});
+		});
+	});
 
 	test.todo('only one fetch is done');
 });
